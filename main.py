5f70474f081163a709ec42a59ac4f4fdde4e86f3
chenlida <ChenLD3124@163.com>
2022-05-26 17:48:26 +0800

    3 ok

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from Env1_Normal import Env1_cal
 from Env2_Freeze import Env2_cal
+from Env3_Sea import Env3_cal
 
-t=-8
-s=0.8
-dt=11
-y=Env2_cal(s,t,dt)
+t=22
+l=12
+h=17
+a=17
+d=0.05
+y=Env3_cal(d,t,h,l,a)
 print(y)
